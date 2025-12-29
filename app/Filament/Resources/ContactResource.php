c971e4e212038d50f0ecb435fad6602eff37b612
sherzot <102805587+sherzot@users.noreply.github.com>
2025-12-29 20:17:57 +0900

    fix: Add Builder import to ContactResource

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -9,6 +9,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class ContactResource extends Resource
 {
